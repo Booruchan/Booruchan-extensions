20c48e6b21e7774910235208a560aca164015b3b
Maxim_Khvostov <mkliverout@gmail.com>
2023-12-27 13:50:53 +0200

    feature: implement tag type for autocomplete entity

--- sdk/src/main/java/org/booruchan/extension/sdk/entity/NetworkAutocomplete.kt
+++ sdk/src/main/java/org/booruchan/extension/sdk/entity/NetworkAutocomplete.kt
@@ -5,6 +5,6 @@ interface NetworkAutocomplete {
     val title: String
     val value: String
     val count: Int
-    /** Represents tag type, but can be empty if type is not defined */
+    /** Represents tag type, but can be Other if type is not defined */
     val type: NetworkTagType
 }
