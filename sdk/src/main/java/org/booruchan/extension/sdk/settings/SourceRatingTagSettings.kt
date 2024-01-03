4422e4ede9e7c05f2420142d1d01d78e3bf06be5
Maxim_Khvostov <mkliverout@gmail.com>
2024-01-03 15:27:08 +0200

    refactor: update rating tag settings docs

--- sdk/src/main/java/org/booruchan/extension/sdk/settings/SourceRatingTagSettings.kt
+++ sdk/src/main/java/org/booruchan/extension/sdk/settings/SourceRatingTagSettings.kt
@@ -1,14 +1,20 @@
 package org.booruchan.extension.sdk.settings
 
-/** Settings for rating metadata tag for booru source types */
+/** Settings for rating tag for booru source types */
 data class SourceRatingTagSettings(
+
     /**
-     * Values for "rating" meta tag (rating:safe, rating:explicit for example)
-     * This values can be used for improving ux in searching
+     * Values for "rating" tag (rating:safe, rating:explicit for example)
+     * This values can be used for improving ux in searching.
+     *
+     * Note: Android app currently supports no more than first 3 items.
+     * Other items will be ignored.
      * */
     val values: List<String> = emptyList(),
+
     /** Rating tag name that should be used for forming proper rating tag */
     val name: String = "rating",
+
     /** Separator for rating tag (rating:safe or rating:general) */
     val tagKeyValueSeparator: String = ":",
 )
