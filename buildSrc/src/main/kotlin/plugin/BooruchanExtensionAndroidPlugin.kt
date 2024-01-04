b517329b1a570cb1ed5d801f5cc0c25b43e58fb7
Maxim_Khvostov <mkliverout@gmail.com>
2024-01-04 17:14:50 +0200

    build: fix gradle tasks for android plugin

--- buildSrc/src/main/kotlin/plugin/BooruchanExtensionAndroidPlugin.kt
+++ buildSrc/src/main/kotlin/plugin/BooruchanExtensionAndroidPlugin.kt
@@ -2,14 +2,24 @@ package plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Exec
 import org.gradle.api.tasks.GradleBuild
 import org.gradle.kotlin.dsl.register
 import task.GenerateCodegenContext
 import task.GenerateCodegenProject
 import java.io.File
+import java.util.*
 
 /** Provides several gradle tasks for generating extensions and installing them on android device */
 class BooruchanExtensionAndroidPlugin : Plugin<Project> {
+
+    private val osName = System.getProperty("os.name").lowercase(Locale.getDefault())
+
+    /** Gradle wrapper script based on OS */
+    private val gradleWrapper: String = when {
+        osName.contains("windows") -> "gradlew.bat"
+        else -> "gradlew"
+    }
 
     override fun apply(project: Project) {
         // For generating context for inflating template files
@@ -21,27 +31,27 @@ class BooruchanExtensionAndroidPlugin : Plugin<Project> {
             dependsOn("generateCodegenContext")
         }
 
-        project.tasks.register<GradleBuild>("assembleAndroidDebug") {
-            dir = File(project.buildDir, "templates${File.separator}android")
-            tasks = listOf(":app:assembleDebug")
+        project.tasks.register<Exec>("assembleAndroidDebug") {
+            workingDir = File(project.buildDir, "templates${File.separator}android")
+            commandLine(File(project.rootDir, gradleWrapper).absolutePath, ":app:assembleDebug")
             dependsOn("generateAndroidProject")
         }
 
-        project.tasks.register<GradleBuild>("assembleAndroidRelease") {
-            dir = File(project.buildDir, "templates${File.separator}android")
-            tasks = listOf(":app:assembleRelease")
+        project.tasks.register<Exec>("assembleAndroidRelease") {
+            workingDir = File(project.buildDir, "templates${File.separator}android")
+            commandLine(File(project.rootDir, gradleWrapper).absolutePath, ":app:assembleRelease")
             dependsOn("generateAndroidProject")
         }
 
-        project.tasks.register<GradleBuild>("installAndroidRelease") {
-            dir = File(project.buildDir, "templates${File.separator}android")
-            tasks = listOf(":app:installRelease")
+        project.tasks.register<Exec>("installAndroidDebug") {
+            workingDir = File(project.buildDir, "templates${File.separator}android")
+            commandLine(File(project.rootDir, gradleWrapper).absolutePath, ":app:installDebug")
             dependsOn("generateAndroidProject")
         }
 
-        project.tasks.register<GradleBuild>("installAndroidDebug") {
-            dir = File(project.buildDir, "templates${File.separator}android")
-            tasks = listOf(":app:installDebug")
+        project.tasks.register<Exec>("installAndroidRelease") {
+            workingDir = File(project.buildDir, "templates${File.separator}android")
+            commandLine(File(project.rootDir, gradleWrapper).absolutePath, ":app:installRelease")
             dependsOn("generateAndroidProject")
         }
     }
