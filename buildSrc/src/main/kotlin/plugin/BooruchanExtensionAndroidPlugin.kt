323dfaacb77431f4af89ac2f737e5e6e45e625fa
Maxim_Khvostov <mkliverout@gmail.com>
2024-03-05 19:16:40 +0200

    ci(github): add properties

--- buildSrc/src/main/kotlin/plugin/BooruchanExtensionAndroidPlugin.kt
+++ buildSrc/src/main/kotlin/plugin/BooruchanExtensionAndroidPlugin.kt
@@ -77,6 +77,7 @@ class BooruchanExtensionAndroidPlugin : Plugin<Project> {
         }
 
         project.tasks.register<Exec>("assembleSignedAndroidRelease") {
+            println("Environment: ${System.getenv("ANDROID_SDK_ROOT")}")
             val properties = Properties().apply { load(project.rootProject.file("local.properties").reader()) }
             val sdkDirectory = File(properties["sdk.dir"].toString())
             val apksigner = sdkDirectory.walkTopDown().find { it.isFile && it.nameWithoutExtension == "apksigner" }
