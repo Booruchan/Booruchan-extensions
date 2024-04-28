2a33c9a34c7a319ee344550fcfeedb6713782a81
Maxim_Khvostov <mkliverout@gmail.com>
2024-04-28 19:17:11 +0300

    sas

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     `java-library`
-//    `maven-publish`
     kotlin("jvm")
     id("com.github.johnrengelman.shadow") version ("7.1.1")
 }
@@ -24,16 +23,3 @@ tasks.test {
 kotlin {
     jvmToolchain(17)
 }
-//afterEvaluate {
-//    // Configure the publication to Maven repository
-//    publishing {
-//        publications {
-//            create<MavenPublication>("maven") {
-//                groupId = "com.booruchan"
-//                artifactId = "sdk"
-//                version = "0.3.5"
-//                from(components["java"])
-//            }
-//        }
-//    }
-//}
