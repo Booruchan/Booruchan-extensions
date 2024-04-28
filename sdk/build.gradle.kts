c531878f1b1e6a304fc94f6e484d9aa156f184ae
Maxim_Khvostov <mkliverout@gmail.com>
2024-04-28 14:58:53 +0300

    build: update publishing tasks for jitpack

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     `java-library`
     kotlin("jvm")
     id("com.github.johnrengelman.shadow") version ("7.1.1")
+    id("maven-publish")
 }
 
 version = "0.3.5"
@@ -22,4 +23,32 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            // Library Package Name
+            // NOTE : Different GroupId For Each Library / Module, So That Each Library Is Not Overwritten
+            groupId = "org.booruchan.extensions"
+
+            // Library Name / Module Name
+            // NOTE : Different ArtifactId For Each Library / Module, So That Each Library Is Not Overwritten
+            artifactId = name
+
+            // Version Library Name (Example : "1.0.0")
+            version = version.toString()
+
+            afterEvaluate {
+                from(components["java"])
+            }
+        }
+    }
 }
