024b67367e61e54f164e910c93b66032a8a9f30e
Maxim_Khvostov <mkliverout@gmail.com>
2024-04-28 15:30:44 +0300

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,42 @@ allprojects {
     group = "org.booruchan.extensions"
 }
 
+subprojects {
+    val subprojectVersion = this.version.toString()
+    val subprojectArtifact = this.name
+
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "maven-publish")
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        withSourcesJar()
+        withJavadocJar()
+    }
+
+    publishing {
+        publications {
+            register<MavenPublication>("maven") {
+                // Library Package Name
+                // NOTE : Different GroupId For Each Library / Module, So That Each Library Is Not Overwritten
+                groupId = "org.booruchan.extensions"
+
+                // Library Name / Module Name
+                // NOTE : Different ArtifactId For Each Library / Module, So That Each Library Is Not Overwritten
+                artifactId = subprojectArtifact
+
+                // Version Library Name (Example : "1.0.0")
+                version = subprojectVersion
+
+                afterEvaluate {
+                    from(components["java"])
+                }
+            }
+        }
+    }
+}
+
 // Plugin for preparing apks for publishing
 // Responsible for collecting apks, aligning and signing,
 // Creating json metadata about whole bunch of apks
