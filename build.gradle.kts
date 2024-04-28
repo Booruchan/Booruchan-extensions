3f990c7941718ce243a457910fd177d1b86c1a22
Maxim_Khvostov <mkliverout@gmail.com>
2024-04-28 15:42:48 +0300

    remove all publishing plugin configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
 import com.booruchan.buildsrc.plugin.BooruchanDeployAndroidPlugin
 
-buildscript {
-    dependencies {
-        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
-    }
-}
-
 plugins {
     kotlin("jvm") version "1.7.20"
     `maven-publish`
@@ -13,42 +7,6 @@ plugins {
 
 allprojects {
     group = "org.booruchan.extensions"
-}
-
-subprojects {
-    val subprojectVersion = this.version.toString()
-    val subprojectArtifact = this.name
-
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-    apply(plugin = "maven-publish")
-
-    java {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-        withSourcesJar()
-        withJavadocJar()
-    }
-
-    publishing {
-        publications {
-            register<MavenPublication>("maven") {
-                // Library Package Name
-                // NOTE : Different GroupId For Each Library / Module, So That Each Library Is Not Overwritten
-                groupId = "org.booruchan.extensions"
-
-                // Library Name / Module Name
-                // NOTE : Different ArtifactId For Each Library / Module, So That Each Library Is Not Overwritten
-                artifactId = subprojectArtifact
-
-                // Version Library Name (Example : "1.0.0")
-                version = subprojectVersion
-
-                afterEvaluate {
-                    from(components["java"])
-                }
-            }
-        }
-    }
 }
 
 // Plugin for preparing apks for publishing
