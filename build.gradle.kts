5dab3145992d59ac706d794dbfc6a0af6763b467
Maxim_Khvostov <mkliverout@gmail.com>
2024-04-28 19:43:24 +0300

    ttestt

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ subprojects {
     apply(plugin = "maven-publish")
 
     java {
+
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
         withSourcesJar()
